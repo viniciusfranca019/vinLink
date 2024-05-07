96858c9882b49c86ef8cc86bce1eda3192219974
viniciusfranca019 <viniciusfrancas019@gmail.com>
2024-05-06 23:13:32 -0300

    change reference

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"vinLink/web/Link"
-	"vinLink/web/Storage"
+	"vinLink/api/Link"
+	"vinLink/api/Storage"
 )
 
 func main() {
@@ -14,7 +14,6 @@ func main() {
 	link := Link.New(fmt.Sprintf("test.com/%v", rand.Intn(5000)))
 
 	linkList[link.Id()] = link.Link()
-
 
 	storage.SaveData(linkList)
 
